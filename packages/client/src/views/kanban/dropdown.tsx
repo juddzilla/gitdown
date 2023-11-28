3b7f9dfe15302fc856b97fa7106d7f41ab1a0a92
Judd Hendrix <juddhendrix@gmail.com>
2023-11-27 17:32:27 -0800

    Update --> d2: two

---
+++ packages/client/src/views/kanban/dropdown.tsx
@@ -0,0 +1,17 @@
+const actionButton = 'capitalize py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none';
+const container = 'hs-dropdown relative inline-flex mr-4';
+const input = 'outline-none p-2 w-full';
+const inputContainer = 'border-b-2 border-slate-200 mb-2';
+const menu = 'border border-gray-200 absolute top-12 transition-[opacity,margin] duration min-w-[15rem] bg-white shadow-md rounded-lg p-2 mt-2';
+const menuItemContainer = 'overflow-scroll w-48 max-h-64 relative -left-px w-full';
+const menuItem = 'flex cursor-pointer w-full items-center gap-x-3.5 py-2 px-3 odd:bg-slate-100 eve:bg-slate-300 rounded-lg text-sm text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-100';
+
+export default {
+  actionButton,
+  container,
+  input,
+  inputContainer,
+  menu,
+  menuItem,
+  menuItemContainer,
+}
